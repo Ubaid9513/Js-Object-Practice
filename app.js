9c798e8628fc3aa3adc3758c52ad2c7218f0b503
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-06-03 00:35:48 +0500

    Updated

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var student = {
-    name : "Ubaid",
-    subject : "Coding",
-    batch : "11",
-    marks : "77",
-    calculatorFees : () => {
-        console.log("Hello, Guys");
-    }
-}
-console.log("Student name :", student.name);
-console.log("Student name :", student["name"]); //Dynamicali access krne k liye
+// var student = {
+//     name : "Ubaid",
+//     subject : "Coding",
+//     batch : "11",
+//     marks : "77",
+//     calculatorFees : () => {
+//         console.log("Hello, Guys");
+//     }
+// }
+// console.log("Student name :", student.name);
+// console.log("Student name :", student["name"]); //Dynamicali access krne k liye
 
-student.calculatorFees()
+// student.calculatorFees()
 
-let country = "country";
-let value  = "Pakistan";
+// let country = "country";
+// let value  = "Pakistan";
 
-student[country] = value;
-console.log(student);
+// student[country] = value;
+// console.log(student);
 
 
 
@@ -37,6 +37,28 @@ console.log(student);
 // 3) kisi function se return ho sake
 
 
+// CONSTRUCTURE FUNCTION 
+
+// function Student(name , roll){
+//     this.name = name,
+//     this.roll = roll
+
+// }
+// console.log(Student);
+// var student1 = new Student("Ubaid",9513);
+// var student2 = new Student("Saqib",9514);
+
+// // ---X---
+
+// var student3 = {
+//     name: "Ubaid",
+//     roll: 123,
+//     age: 18
+// }
+// console.log(student3.hasOwnProperty("age"));
+// console.log("name" in student3);
+
+// console.log(student3);
 
 
 
